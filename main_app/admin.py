9448ed8f1a9e560614fd4dcb9ae4a37cb6bc6c6f
Aqeel Muslem <3qeelmoslem@gmail.com>
2025-09-15 23:04:15 +0300

    add mesage model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Visa
+from .models import Message
 
 admin.site.register(Visa)
+
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('subject', 'sender', 'is_read', 'created_at')
+    list_filter = ('is_read', 'created_at')
+    search_fields = ('subject', 'body', 'sender__username')
--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.conf import settings
 
 class Visa(models.Model):
     full_name = models.CharField(max_length=100)
@@ -17,5 +17,18 @@ class Visa(models.Model):
 
 
 
+class Message(models.Model):
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, 
+        on_delete=models.CASCADE, 
+        related_name='sent_messages'
+    )
+    subject = models.CharField(max_length=255)
+    body = models.TextField()
+    is_read = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.subject} - {self.sender.username}"
     
     
